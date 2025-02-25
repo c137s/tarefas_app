a494ebed30405909b1bd1ec3056dbdf287763990
c137s <sidnei.s.s.f.9@gmail.com>
2025-02-25 19:26:47 -0300

    Primeiro

---
+++ lib/tarefas_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TarefasPage extends StatefulWidget {
+  const TarefasPage({super.key});
+
+  @override
+  State<TarefasPage> createState() => _TarefasPageState();
+}
+
+class _TarefasPageState extends State<TarefasPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

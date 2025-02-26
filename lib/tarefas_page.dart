0c341e49394e0eefe92c7326190ce22f196e7d08
c137s <sidnei.s.s.f.9@gmail.com>
2025-02-26 19:54:32 -0300

    Salvar as notas na memoria

--- lib/tarefas_page.dart
+++ lib/tarefas_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class TarefasPage extends StatefulWidget {
   const TarefasPage({super.key});
@@ -8,8 +9,107 @@ class TarefasPage extends StatefulWidget {
 }
 
 class _TarefasPageState extends State<TarefasPage> {
+
+  late SharedPreferences prefs;
+
+List<String> tarefas = [
+  "Pagar Contaras",
+  "Comprar Agua"
+];
+
+String novaTarefa = "";
+
+void addTarefa(){
+if(novaTarefa != ""){
+setState(() {
+    tarefas.add(novaTarefa);
+ });
+ prefs.setStringList("tarefas", tarefas);
+}
+}
+
+void removeTarefa(String tarefa){
+  setState(() {
+    tarefas.remove(tarefa);
+ });
+prefs.setStringList("tarefas", tarefas);
+}
+
+@override
+  void initState() {
+    super.initState();
+    carregarTarefas();
+  }
+
+ Future<void> carregarTarefas() async {
+prefs = await SharedPreferences.getInstance();
+setState(() {
+  tarefas = prefs.getStringList("tarefas")??[];
+});
+ }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text("Notas"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: ListView(
+          children: [
+           ...tarefas.map((tarefa)=>  GestureDetector(
+            onTap: (){
+              removeTarefa(tarefa);
+            },
+             child: Card(
+                child: Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(tarefa))),
+           ))
+           
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        height: 60,
+         color: Colors.teal,
+         child: Padding(
+           padding: const EdgeInsets.all(8.0),
+           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: 400,
+                child: TextFormField(
+                  onChanged: (valor){
+                    novaTarefa = valor;
+                  },
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder()
+                  ),
+                )),
+              GestureDetector(
+                
+                onTap: (){
+                addTarefa();
+                },
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  color: Colors.black38,
+                  child: Icon(Icons.add),
+                ),
+              )
+            ],
+           ),
+         ),
+      ),
+
+      );
   }
 }
